7ac107ac8f8c02a580d5e670975489c42c2a20fd
Avez-Anwar <34218292+Avez-Anwar@users.noreply.github.com>
2021-06-01 18:07:37 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: [],
+      error: "",
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => {
+          this.setState({ post: response.data });
+          this.setState({ error: "" });
+          this.setState({ loading: false });
+        })
+        .catch(() => {
+          console.log("error");
+          this.setState({ post: [] });
+          this.setState({ error: "Something went wrong" });
+          this.setState({ loading: false });
+        });
+    }, 2000);
+  }
+
+  render() {
+    const newPost = this.state.post.map(function (posts) {
+      return <li key={posts.id}>{posts.body}</li>;
+    });
+
+    return (
+      <div className="App">
+        {this.state.loading ? "Loading..." : newPost}
+        {this.state.error ? "Something went wrong..." : null}
+      </div>
+    );
+  }
 }
 
 export default App;
